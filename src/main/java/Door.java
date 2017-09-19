89eaf25ea92d6c7f0c0b4849c4bad0343eb0366f
sklein <simon.klein.sz.1@gmail.com>
2017-09-19 11:01:25 +0200

    Finished Programm to Calculate the Chance

---
+++ src/main/java/Door.java
@@ -0,0 +1,59 @@
+import java.util.Random;
+
+public class Door {
+    private static final int ZONK = 0;
+    private static final int WIN = 1;
+
+    public static final int SELECTABLE = 0;
+    public static final int SELECTED = 1;
+    public static final int OPENED = 2;
+
+    private int statusOfSelection;
+    private int valueBehindTheDoor;
+
+    private Door(){
+        statusOfSelection = SELECTABLE;
+        valueBehindTheDoor = ZONK;
+    }
+
+    public boolean isSelected() {
+        return statusOfSelection == SELECTED;
+    }
+
+    public boolean isSelectable() {
+        return statusOfSelection == SELECTABLE;
+    }
+
+    public boolean isOpened() {
+        return statusOfSelection == OPENED;
+    }
+
+    public boolean isWin(){
+        return valueBehindTheDoor == WIN;
+    }
+
+    public void setStatusOfSelection(int statusOfSelection) {
+        this.statusOfSelection = statusOfSelection;
+    }
+
+    public void setValueBehindTheDoor(int valueBehindTheDoor) {
+        this.valueBehindTheDoor = valueBehindTheDoor;
+    }
+
+    public static Door[] getThreeDoors(){
+        Door[] doors = new Door[3];
+        for (int i = 0; i < doors.length; i++) {
+            doors[i] = new Door();
+        }
+        Random random = new Random();
+
+        int doorToSetWinInto = random.nextInt(3);
+        doors[doorToSetWinInto].setValueBehindTheDoor(WIN);
+
+        return doors;
+    }
+
+
+
+
+}
